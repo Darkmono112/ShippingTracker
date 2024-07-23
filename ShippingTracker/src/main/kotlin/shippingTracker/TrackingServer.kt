3128ea0944aa6d8e89595565d984b99b7326f133
Robert Gordon <darkmono37@gmail.com>
2024-07-22 18:41:27 -0600

    added server capability

--- ShippingTracker/src/main/kotlin/shippingTracker/TrackingServer.kt
+++ ShippingTracker/src/main/kotlin/shippingTracker/TrackingServer.kt
@@ -4,6 +4,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
@@ -18,6 +19,11 @@ public class TrackingServer() {
             routing {
                 get("/") {
                     call.respondText(File("index.html").readText(), ContentType.Text.Html)
+                }
+                post("/data"){
+                    var data = call.receiveText()
+                    println(data)
+                    call.respondText { "Data sent to server" }
                 }
             }
         }.start(wait = false )
